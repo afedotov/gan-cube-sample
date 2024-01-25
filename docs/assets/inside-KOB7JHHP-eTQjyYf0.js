8aff0631ff8e11197294138747f2f1473680d95b
Andy Fedotov <andy.fedotov@gmail.com>
2024-01-26 00:33:05 +0300

    Bump packages, update Chrome flag name, minor UI fixes

---
+++ docs/assets/inside-KOB7JHHP-eTQjyYf0.js
@@ -0,0 +1,1 @@
+import{e as o}from"./search-worker-entry-h1lfYyNX.js";import{w as e}from"./index-5PRf6e5S.js";o(e);
--- docs/assets/inside-SUMDRTDV-czDYVOox.js
+++
@@ -1,1 +0,0 @@
-import{e as o}from"./search-worker-entry-0g93ViaF.js";import{w as e}from"./index-logemQSe.js";o(e);
