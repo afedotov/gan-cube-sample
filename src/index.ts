42181a952c0784240dfdb3d37d166cf1e20ba404
Andy Fedotov <andy.fedotov@gmail.com>
2024-02-12 22:30:52 +0300

    Bump packages, gan-web-bluetooth 2.2.0

--- src/index.ts
+++ src/index.ts
@@ -140,9 +140,13 @@ function handleCubeEvent(event: GanCubeEvent) {
   }
 }
 
-const customMacAddressProvider: MacAddressProvider = async (device): Promise<string | null> => {
-  return typeof device.watchAdvertisements == 'function' ? null :
-    prompt('Seems like your browser does not support Web Bluetooth watchAdvertisements() API. Enable following flag in Chrome:\n\nchrome://flags/#enable-experimental-web-platform-features\n\nor enter cube MAC address manually:');
+const customMacAddressProvider: MacAddressProvider = async (device, isFallbackCall): Promise<string | null> => {
+  if (isFallbackCall) {
+    return prompt('Unable do determine cube MAC address!\nPlease enter MAC address manually:');
+  } else {
+    return typeof device.watchAdvertisements == 'function' ? null :
+      prompt('Seems like your browser does not support Web Bluetooth watchAdvertisements() API. Enable following flag in Chrome:\n\nchrome://flags/#enable-experimental-web-platform-features\n\nor enter cube MAC address manually:');
+  }
 };
 
 $('#reset-state').on('click', async () => {
